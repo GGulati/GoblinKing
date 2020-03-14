3cf9e8e5a67e8cc43618c85207df2c8b080f83eb
GGulati <2336395+GGulati@users.noreply.github.com>
2020-03-14 08:12:39 +0000

    Deploying to gh-pages from  - 1592bbe89c67f048d33fa25bd43ef21ad6edfc05 🚀

---
+++ precache-manifest.f1890f4b3c6f8eb0cdda628b8405ec15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8066073e730cb244d212d03db74eda9c",
+    "url": "/GoblinKing/index.html"
+  },
+  {
+    "revision": "f1950d35f895dc8e24c2",
+    "url": "/GoblinKing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc5b8ff4cfea40c9f134",
+    "url": "/GoblinKing/static/js/2.e61bee81.chunk.js"
+  },
+  {
+    "revision": "d4c4cac17c7c59ed1147ef44c709cbcd",
+    "url": "/GoblinKing/static/js/2.e61bee81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1950d35f895dc8e24c2",
+    "url": "/GoblinKing/static/js/main.380e0740.chunk.js"
+  },
+  {
+    "revision": "e8b58dedcba3d3be4294",
+    "url": "/GoblinKing/static/js/runtime-main.487813a1.js"
+  }
+]);
